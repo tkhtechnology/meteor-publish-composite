3e24ade5d7fe63b309eee286da1259981709e0a3
Robert Ratajczak <r.ratajczak@tkhtechnology.com>
2025-12-15 12:21:20 +0100

    release

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name: 'cctech:publish-composite',
   summary: 'Publish a set of related documents from multiple collections with a reactive join.',
-  version: '1.9.1-dev.5',
+  version: '1.9.1-dev.6',
   git: 'https://github.com/tkhtechnology/meteor-publish-composite'
 })
 
